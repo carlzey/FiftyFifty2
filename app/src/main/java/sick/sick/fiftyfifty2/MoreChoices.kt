545910bb5eb2a29f03b30cc5f46d5cd9db6ecef0
carlzey <carlzeybrandt@gmail.com>
2025-05-21 20:11:20 +0200

    Fixat MoreChoices

--- app/src/main/java/sick/sick/fiftyfifty2/MoreChoices.kt
+++ app/src/main/java/sick/sick/fiftyfifty2/MoreChoices.kt
@@ -1,6 +1,7 @@
 package sick.sick.fiftyfifty2
 
 
+import android.R.attr.title
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.view.RoundedCorner
@@ -13,14 +14,27 @@ import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ScrollableTabRow
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
@@ -54,23 +68,84 @@ fun moreChoicesView(viewModel: HistoryViewModel) {
     var valdText = remember { mutableStateOf("") } // Vald Text för att skicka till Motor
     val context = LocalContext.current // Kontext för att starta motor
 
+
+    Box(modifier = Modifier.fillMaxSize()) {
+
+        val scrollState = rememberScrollState()
+        // Få rätt höjd för scrollaren och visa den
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .offset(y = (-220).dp)
+                .height(130.dp)
+                .verticalScroll(scrollState, reverseScrolling = true)
+        ) {
+
+            // Flowrow för att visa Design Så det blir snyggt
+            FlowRow(
+                mainAxisSpacing = 8.dp,
+                crossAxisSpacing = 8.dp,
+            )
+            {
+                choices.forEach { choice -> // Loopa genom valda alternativ och visa dem
+
+                    // För att få snygg design
+                    Surface(
+                        color = MaterialTheme.colorScheme.primaryContainer,
+                        shape = RoundedCornerShape(12.dp),
+                        tonalElevation = 2.dp // Snygghet hur det står
+                    ) {
+                        Row(
+                            modifier = Modifier.padding(
+                                horizontal = 12.dp,
+                                vertical = 8.dp
+                            ),
+                            verticalAlignment = Alignment.CenterVertically // bottom förut
+                        ) {
+                            Text(
+                                choice,
+                                fontSize = 15.sp,
+                                color = MaterialTheme.colorScheme.primary,
+                                fontWeight = androidx.compose.ui.text.font.FontWeight.ExtraBold,
+                                modifier = Modifier.padding(end = 4.dp)
+                            )
+                            IconButton(
+                                onClick = { choices.remove(choice) },
+                                modifier = Modifier.size(16.dp)
+                            ) {
+                                Icon( // kanske ändra till Icons.Default.Close
+                                    imageVector = androidx.compose.material.icons.Icons.Default.Close,
+                                    contentDescription = "Remove",
+                                    modifier = Modifier.size(12.dp),
+                                    tint = Red
+                                )
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+    // Ha den centralt och ligga där hela tiden så den inte försvinner
     Column (
-        modifier = Modifier.fillMaxSize().padding(16.dp).padding(bottom = 100.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .padding(bottom = 100.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text("More Choices", fontSize = 50.sp) // Titeln
-
         TextField(
             value = inText.value,
             onValueChange = { inText.value = it }, // Uppdaterar inText varje gång användaren skriver
             keyboardOptions = KeyboardOptions.Default.copy( // Stor bokstav
                 capitalization = androidx.compose.ui.text.input.KeyboardCapitalization.Sentences
             ),
-            label = { Text("Add Choices") }, // Hint
+            label = { Text("Choice") }, // Hint
             modifier = Modifier.padding(top = 8.dp) // Lägger till padding ovan
         )
-        Row(
+        Row( // kanske spaced by 8 dp horizontellt
             horizontalArrangement = Arrangement.Center,
             modifier = Modifier.padding(top = 8.dp) // Lägger till padding ovan
         ) {
@@ -82,7 +157,7 @@ fun moreChoicesView(viewModel: HistoryViewModel) {
                     // inputText = "" // Nollställ fältet efter tillägg
                 }
             }) {
-                Text("Add") // Lägg till
+                Text("Add Choice") // Lägg till
             } // Slut på row
             Button(onClick = { choices.clear() }) { // Tömmer lista
                 Text("Clear")
@@ -100,7 +175,7 @@ fun moreChoicesView(viewModel: HistoryViewModel) {
                         viewModel.insertHistory("MoreChoices", joined, selectedChoice, currentDate) // Spara resultat i Historik
 
                         //Starta aktivitet
-                        val intent = Intent(context, Motor::class.java).apply { 
+                        val intent = Intent(context, Motor::class.java).apply {
                             putExtra(
                                 "EXTRA_MESSAGE",
                                 selectedChoice
@@ -114,33 +189,12 @@ fun moreChoicesView(viewModel: HistoryViewModel) {
 
                     }
                 },
-                modifier = Modifier.padding(top = 2.dp)
+                modifier = Modifier.padding(top = 2.dp), // kanske ändra till 8
+                enabled = choices.isNotEmpty() // Om choices inte är tom så är knappen inaktivt
 
             ) {
                 Text("Randomize!")
             }
-        // Flowrow för att visa Design Så det blir snyggt
-        FlowRow(
-            mainAxisSpacing = 8.dp,
-            crossAxisSpacing = 8.dp,
-            modifier = Modifier.padding(top = 8.dp)
-            ,
-
-        )
-            {
-            choices.forEach { choice -> // Loopa genom valda alternativ och visa dem
-                Box(
-                    modifier =
-                    Modifier.background(MaterialTheme.colorScheme.primaryContainer).clip(RoundedCornerShape(12.dp))
-                        .border(1.dp, MaterialTheme.colorScheme.primary, RoundedCornerShape(12.dp))
-                        .padding(horizontal = 12.dp, vertical = 12.dp)
-                ) {
-                    Text(
-                        choice, fontSize = 10.sp, color = MaterialTheme.colorScheme.primary
-                    )
-                }
-            }
-        }
         }
     }
 
